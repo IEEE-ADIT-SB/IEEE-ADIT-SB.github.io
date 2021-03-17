7cad2ab7695662ca290000405f8044b6745cfbd9
Aditya Gaurav <64150766+adityagaurav28@users.noreply.github.com>
2021-03-17 13:59:54 +0530

    Add files via upload

--- JS/script.js
+++ JS/script.js
@@ -5,3 +5,22 @@ $('#wave').wavify({
     color: '#111',
     speed: .25
 });
+
+var swiper = new Swiper('.yearCard', {
+    effect: 'cube',
+    loop: true,
+    cubeEffect: {
+      shadow: true,
+      slideShadows: true,
+      shadowOffset: 20,
+      shadowScale: 0.94,
+    },
+    pagination: {
+      el: '.slideNav',
+      clickable: true,
+    },
+    autoplay: {
+        delay: 20000,
+        disableOnInteraction: false,
+    },
+});
